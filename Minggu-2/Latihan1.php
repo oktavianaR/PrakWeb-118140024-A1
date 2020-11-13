8f84cc0a26b4638876646eb6752478e5025cdba9
Oktaviana Rinda Sari <65061216+oktavianaR@users.noreply.github.com>
2020-11-13 16:34:52 +0700

    Ini Latihan1 Praktikum 2

---
+++ Minggu-2/Latihan1.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan 1 Kalkulator</title>
+</head>
+<body>
+<?php
+    $a = 20;
+    $b = 5;
+    echo 'Bil 1 = '.$a;
+    echo '<br>';
+    echo 'Bil 2 = '.$b;
+    echo '<br>';
+    echo '<br>';
+    echo 'Hasil dari setiap operasi bilangan';
+    echo '<br>';
+    echo '<br>';
+    echo 'PENJUMLAHAN';
+    echo '<br>';
+    echo 'Operator : +';
+    echo '<br>';
+    echo "Hasil : ".($a+$b);
+    echo '<br>';
+    echo '<br>';
+    echo 'PENGURANGAN';
+    echo '<br>';
+    echo 'Operator : -';
+    echo '<br>';
+    echo "Hasil : ". ($a-$b);
+    echo '<br>';
+    echo '<br>';
+    echo 'PERKALIAN';
+    echo '<br>';
+    echo 'Operator : *';
+    echo '<br>';
+    echo "Hasil : ".$a*$b;
+    echo '<br>';
+    echo '<br>';
+    echo 'PEMBAGIAN';
+    echo '<br>';
+    echo 'Operator : /';
+    echo '<br>';
+    echo "Hasil : ".$a/$b;
+    echo '<br>';
+    echo '<br>';
+    echo 'MODULUS';
+    echo '<br>';
+    echo 'Operator : %';
+    echo '<br>';
+    echo "Hasil : ".$a%$b;
+    echo '<br>';
+    echo '<br>';
+?>
+</body>
+</html>
